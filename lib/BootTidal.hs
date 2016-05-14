c6a176df0f97f68e562d61b7bd9b38b45b3fc0d2
Alex McLean <alex@slab.org>
2016-05-14 17:33:00 +0100

    Boot with both dirt and superdirt support

--- lib/BootTidal.hs
+++ lib/BootTidal.hs
@@ -13,8 +13,19 @@
 (d8,t8) <- dirtSetters getNow
 (d9,t9) <- dirtSetters getNow
 
+(s1,ts1) <- superDirtSetters getNow
+(s2,ts2) <- superDirtSetters getNow
+(s3,ts3) <- superDirtSetters getNow
+(s4,ts4) <- superDirtSetters getNow
+(s5,ts5) <- superDirtSetters getNow
+(s6,ts6) <- superDirtSetters getNow
+(s7,ts7) <- superDirtSetters getNow
+(s8,ts8) <- superDirtSetters getNow
+(s9,ts9) <- superDirtSetters getNow
+
+
 let bps x = cps (x/2)
-let hush = mapM_ ($ silence) [d1,d2,d3,d4,d5,d6,d7,d8,d9]
+let hush = mapM_ ($ silence) [d1,d2,d3,d4,d5,d6,d7,d8,d9,s1,s2,s3,s4,s5,s6,s7,s8,s9]
 let solo = (>>) hush
 
 :set prompt "tidal> "
